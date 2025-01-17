171d686c540b82c682c3736dc37cbf59feb92499
PhucNg-009876 <Pppp009876@gmail.com>
2025-01-17 19:10:46 +0700

    complete-video1

---
+++ src/components/backoffice/CustomDataTable.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CustomDataTable() {
+  return (
+    <div className="">
+      <h2 className="text-xl font-bold mb-4">table</h2>
+    </div>
+  );
+}
